7601e450ff844498f97fa8d2d85d9903de4210ea
Lloyd Prock <lloyd.prock@gmail.com>
2022-07-23 13:36:04 -0500

    Split Numbers and Operators

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const calcBtns = document.querySelectorAll(".calcBtn");
+const calcBtnNumber = document.querySelectorAll(".calcBtnNumber");
+const calcBtnOperator = document.querySelectorAll(".calcBtnOperator");
 const calcDisplay = document.querySelector(".calcDisplay");
+const clear = document.querySelector("#clear");
+
 let currentDisplay = parseInt(calcDisplay.innerText);
 
-for (let i = 0; i < calcBtns.length; i++) {
-    calcBtns[i].addEventListener("click", () => {
-        if (parseInt(calcBtns[i].innerText) || parseInt(calcBtns[i].innerText) === 0) {
+//Need to figure out how to do a max length on the display that is not a set number
+//Doing math may require a variable that keeps adding on the calculators requests and then equal will runt he calculation?
+
+for (let i = 0; i < calcBtnNumber.length; i++) {
+    calcBtnNumber[i].addEventListener("click", () => {
+        if (parseInt(calcBtnNumber[i].innerText) || parseInt(calcBtnNumber[i].innerText) === 0) {
             if (currentDisplay === 0) {
                 if (calcDisplay.innerText.length < 10) {
-                    currentDisplay = parseInt(calcBtns[i].innerText);
+                    currentDisplay = parseInt(calcBtnNumber[i].innerText);
                     calcDisplay.innerText = currentDisplay;
                 }
             } else {
                 if (calcDisplay.innerText.length < 10) {
-                    currentDisplay = currentDisplay + calcBtns[i].innerText;
+                    currentDisplay = currentDisplay + calcBtnNumber[i].innerText;
                     calcDisplay.innerText = currentDisplay;
                 }
             }
@@ -20,7 +26,6 @@ for (let i = 0; i < calcBtns.length; i++) {
     })
 }
 
-const clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
     currentDisplay = 0;
     calcDisplay.innerText = currentDisplay;
