829fb8f69f548fb2ca706dedfc28d97da052da80
Lloyd Prock <lloyd.prock@gmail.com>
2022-07-22 23:42:21 -0500

    Working Number and Clear

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const calcBtns = document.querySelectorAll(".calcBtn");
 const calcDisplay = document.querySelector(".calcDisplay");
+let currentDisplay = parseInt(calcDisplay.innerText);
 
 for (let i = 0; i < calcBtns.length; i++) {
     calcBtns[i].addEventListener("click", () => {
         if (parseInt(calcBtns[i].innerText) || parseInt(calcBtns[i].innerText) === 0) {
-            let currentDisplay = calcDisplay.innerText;
-            if (calcDisplay.innerText.length < 10) {
-                calcDisplay.innerText = currentDisplay + calcBtns[i].innerText;
-                console.log(calcDisplay.innerText)
+            if (currentDisplay === 0) {
+                if (calcDisplay.innerText.length < 10) {
+                    currentDisplay = calcBtns[i].innerText;
+                    calcDisplay.innerText = currentDisplay;
+                }
+            } else {
+                if (calcDisplay.innerText.length < 10) {
+                    currentDisplay = currentDisplay + calcBtns[i].innerText;
+                    calcDisplay.innerText = currentDisplay;
+                }
             }
         }
     })
 }
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    currentDisplay = 0;
+    calcDisplay.innerText = currentDisplay;
+})
